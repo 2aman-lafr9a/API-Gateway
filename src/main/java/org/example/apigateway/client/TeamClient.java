4c341a51c3ebfd7194a83c8de56d8389dedd57e7
BakrAsskali <bakrasskali@gmail.com>
2023-12-27 13:25:29 +0100

    Added my own protos

---
+++ src/main/java/org/example/apigateway/client/TeamClient.java
@@ -0,0 +1,11 @@
+package org.example.apigateway.client;
+
+import com.google.inject.AbstractModule;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+public class TeamClient extends AbstractModule {
+    private static final String HOST = "localhost";
+    private static final int PORT = 8080;
+    
+}
--- src/main/java/org/example/apigateway/service/AuthService.java
+++ src/main/java/org/example/apigateway/service/AuthService.java
@@ -2,4 +2,32 @@ package org.example.apigateway.service;
 
 
 import net.devh.boot.grpc.client.inject.GrpcClient;
+import org.example.authentication.AuthenticationProto;
 import org.springframework.stereotype.Service;
+import org.example.authentication.AuthenticationGrpc;
+
+@Service
+public class AuthService {
+
+    @GrpcClient("authentication-service")
+    private AuthenticationGrpc.AuthenticationBlockingStub authenticationStub;
+
+    public void signIn(String userName, String password) {
+        AuthenticationProto.SignInResponse signInResponse = authenticationStub.signIn(AuthenticationProto.SignInRequest.newBuilder()
+                .setUsername(userName)
+                .setPassword(password)
+                .build());
+
+        System.out.println("Token: " + signInResponse.getJwt());
+    }
+
+    public void signUp(String userName, String password) {
+        AuthenticationProto.SignUpResponse signUpResponse = authenticationStub.signUp(AuthenticationProto.SignUpRequest.newBuilder()
+                .setUsername(userName)
+                .setPassword(password)
+                .build());
+
+        System.out.println("Message: " + signUpResponse.getMessage());
+        System.out.println("Status: " + signUpResponse.getSuccess());
+    }
+}
