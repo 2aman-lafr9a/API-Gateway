65bc0db4de83290664259baa65d263e0838f8bef
sohaib <souhaibemanah@gmail.com>
2024-01-15 23:08:59 +0100

    hard coded ip

--- src/main/java/org/example/apigateway/utils/ResolveHostnameToIP.java
+++ src/main/java/org/example/apigateway/utils/ResolveHostnameToIP.java
@@ -8,8 +8,10 @@ public class ResolveHostnameToIP {
     public static String resolve(String hostname) {
         try {
             InetAddress address = InetAddress.getByName(hostname);
+            System.out.println("Resolved hostname: " + hostname + " to IP: " + address.getHostAddress());
             return address.getHostAddress();
         } catch (UnknownHostException e) {
+            System.out.println("Could not resolve hostname: " + hostname);
             return "127.0.0.1";
         }
     }
