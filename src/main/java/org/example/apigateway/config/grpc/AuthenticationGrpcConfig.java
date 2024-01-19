bd89eba66bc789ef3d7aba25a7adff40eee145c6
BakrAsskali <bakrasskali@gmail.com>
2024-01-19 17:14:45 +0100

    Almost commented all your auth files

--- src/main/java/org/example/apigateway/config/grpc/AuthenticationGrpcConfig.java
+++ src/main/java/org/example/apigateway/config/grpc/AuthenticationGrpcConfig.java
@@ -1,25 +1,25 @@
-//package org.example.apigateway.config.grpc;
-//
-//
-//import org.example.apigateway.grpc.authentication.AuthenticationGrpc;
-//import net.devh.boot.grpc.client.inject.GrpcClient;
-//import net.devh.boot.grpc.client.inject.GrpcClientBean;
-//import org.example.apigateway.service.AuthService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//
-//@Configuration
-//@GrpcClientBean(
-//        clazz = AuthenticationGrpc.AuthenticationBlockingStub.class,
-//        beanName = "AuthService",
-//        client = @GrpcClient("authentication_service")
-//)
-//public class AuthenticationGrpcConfig {
-//    @Bean
-//    AuthService authService(@Autowired AuthenticationGrpc.AuthenticationBlockingStub blockingStub) {
-//        return new AuthService(blockingStub);
-//    }
-//}
-//
+package org.example.apigateway.config.grpc;
+
+
+import org.example.apigateway.grpc.authentication.AuthenticationGrpc;
+import net.devh.boot.grpc.client.inject.GrpcClient;
+import net.devh.boot.grpc.client.inject.GrpcClientBean;
+import org.example.apigateway.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@GrpcClientBean(
+        clazz = AuthenticationGrpc.AuthenticationBlockingStub.class,
+        beanName = "AuthService",
+        client = @GrpcClient("authentication_service")
+)
+public class AuthenticationGrpcConfig {
+    @Bean
+    AuthService authService(@Autowired AuthenticationGrpc.AuthenticationBlockingStub blockingStub) {
+        return new AuthService(blockingStub);
+    }
+}
+
